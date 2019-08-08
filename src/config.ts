cde62e568b273df826fbfa0491bc29a69698339f
Whien <sal95610@gmail.com>
2019-08-08 11:43:32 +0800

    Update: Redis version

---
+++ src/config.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not
+ * use this file except in compliance with the License. You may obtain a copy
+ of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ under
+ * the License.
+ */
+
+'use strict';
+
+import * as fse from 'fs-extra';
+import * as path from 'path';
+
+const CONFIG_PATH = path.resolve(__dirname, '../config.json');
+
+export type Config = {
+  cacheTool: string;
+  datastoreCache: boolean;
+  timeout: number;
+  port: string;
+  width: number;
+  height: number;
+  redisHost: string;
+  redisPort: number;
+  redisExpireTime: number;
+};
+
+export class ConfigManager {
+  public static config: Config = {
+    cacheTool: 'redis',
+    datastoreCache: true,
+    timeout: 10000,
+    port: '3000',
+    width: 1000,
+    height: 1000,
+    redisHost: 'localhost',
+    redisPort: 6379,
+    redisExpireTime: 900
+  };
+
+  static async getConfiguration(): Promise<Config> {
+    // Load config.json if it exists.
+    if (fse.pathExistsSync(CONFIG_PATH)) {
+      ConfigManager.config = Object.assign(
+        ConfigManager.config,
+        await fse.readJson(CONFIG_PATH)
+      );
+    }
+    return ConfigManager.config;
+  }
+}
---
+++ src/rendertron.ts
@@ -0,0 +1,175 @@
+import * as Koa from 'koa';
+import * as bodyParser from 'koa-bodyparser';
+import * as koaCompress from 'koa-compress';
+import * as route from 'koa-route';
+import * as koaSend from 'koa-send';
+import * as koaLogger from 'koa-logger';
+import * as path from 'path';
+import * as puppeteer from 'puppeteer';
+import * as url from 'url';
+
+import { Renderer, ScreenshotError } from './renderer';
+import { Config, ConfigManager } from './config';
+
+/**
+ * Rendertron rendering service. This runs the server which routes rendering
+ * requests through to the renderer.
+ */
+export class Rendertron {
+  app: Koa = new Koa();
+  private config: Config = ConfigManager.config;
+  private renderer: Renderer | undefined;
+  private port = process.env.PORT;
+
+  async initialize() {
+    // Load config
+    this.config = await ConfigManager.getConfiguration();
+
+    this.port = this.port || this.config.port;
+
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    this.renderer = new Renderer(browser, this.config);
+
+    this.app.use(koaLogger());
+
+    this.app.use(koaCompress());
+
+    this.app.use(bodyParser());
+
+    this.app.use(
+      route.get('/', async (ctx: Koa.Context) => {
+        await koaSend(ctx, 'index.html', {
+          root: path.resolve(__dirname, '../src')
+        });
+      })
+    );
+    this.app.use(
+      route.get('/_ah/health', (ctx: Koa.Context) => (ctx.body = 'OK'))
+    );
+
+    if (this.config.datastoreCache) {
+      if (this.config.cacheTool === 'redis') {
+        const { promisify } = await import('util');
+        const redis = await import('redis'),
+          redisClient = redis.createClient({
+            host: (process.env.REDIS_HOST as string) || this.config.redisHost,
+            port:
+              parseInt(process.env.REDIS_PORT as string, 10) ||
+              this.config.redisPort
+          });
+        const getAsync = promisify(redisClient.get).bind(redisClient);
+        this.app.use(
+          async (ctx: Koa.Context, next: () => Promise<undefined>) => {
+            const url = ctx.url;
+            const result = await getAsync(url);
+            if (result) {
+              ctx.status = 200;
+              ctx.body = result;
+              return;
+            }
+
+            await next();
+
+            if (ctx.status === 200) {
+              redisClient.set(url, ctx.body, 'ex', this.config.redisExpireTime);
+            }
+          }
+        );
+      }
+    }
+
+    this.app.use(
+      route.get('/render/:url(.*)', this.handleRenderRequest.bind(this))
+    );
+
+    return this.app.listen(this.port, () => {
+      console.log(`Listening on port ${this.port}`);
+    });
+  }
+
+  /**
+   * Checks whether or not the URL is valid. For example, we don't want to allow
+   * the requester to read the file system via Chrome.
+   */
+  restricted(href: string): boolean {
+    const parsedUrl = url.parse(href);
+    const protocol = parsedUrl.protocol || '';
+
+    if (!protocol.match(/^https?/)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  async handleRenderRequest(ctx: Koa.Context, url: string) {
+    if (!this.renderer) {
+      throw new Error('No renderer initalized yet.');
+    }
+
+    if (this.restricted(url)) {
+      ctx.status = 403;
+      return;
+    }
+
+    const mobileVersion = 'mobile' in ctx.query ? true : false;
+
+    const serialized = await this.renderer.serialize(url, mobileVersion);
+    // Mark the response as coming from Rendertron.
+    ctx.set('x-renderer', 'rendertron');
+    ctx.status = serialized.status;
+    ctx.body = serialized.content;
+  }
+
+  async handleScreenshotRequest(ctx: Koa.Context, url: string) {
+    if (!this.renderer) {
+      throw new Error('No renderer initalized yet.');
+    }
+
+    if (this.restricted(url)) {
+      ctx.status = 403;
+      return;
+    }
+
+    let options = undefined;
+    if (ctx.method === 'POST' && ctx.request.body) {
+      options = ctx.request.body;
+    }
+
+    const dimensions = {
+      width: Number(ctx.query['width']) || this.config.width,
+      height: Number(ctx.query['height']) || this.config.height
+    };
+
+    const mobileVersion = 'mobile' in ctx.query ? true : false;
+
+    try {
+      const img = await this.renderer.screenshot(
+        url,
+        mobileVersion,
+        dimensions,
+        options
+      );
+      ctx.set('Content-Type', 'image/jpeg');
+      ctx.set('Content-Length', img.length.toString());
+      ctx.body = img;
+    } catch (error) {
+      const err = error as ScreenshotError;
+      ctx.status = err.type === 'Forbidden' ? 403 : 500;
+    }
+  }
+}
+
+async function logUncaughtError(error: Error) {
+  console.error('Uncaught exception');
+  console.error(error);
+  process.exit(1);
+}
+
+// Start rendertron if not running inside tests.
+if (!module.parent) {
+  const rendertron = new Rendertron();
+  rendertron.initialize();
+
+  process.on('uncaughtException', logUncaughtError);
+}
